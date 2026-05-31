c36f639ff0b4e43d64b9bed61dea227faef62984
Ilekuba Oluebube Collins <cilekuba.2502996@stu.cu.edu.ng>
2026-05-31 01:31:30 +0100

    chore: remove redundant inspection and bootstrap files

--- _tmp_db_check.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-conn = sqlite3.connect('primechop.db')
-cur = conn.cursor()
-print(cur.execute('SELECT name FROM sqlite_master WHERE type="table" ORDER BY name').fetchall())
-conn.close()
